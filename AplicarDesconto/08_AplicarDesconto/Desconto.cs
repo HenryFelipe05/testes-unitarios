05df399540217b5e6ad4094296162b738f14c862
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-05-16 20:17:18 -0300

    feat: adicionando projeto e testes unitarios

---
+++ AplicarDesconto/08_AplicarDesconto/Desconto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_AplicarDesconto
+{
+    public class Desconto
+    {
+        // Verificar se uma compra pode receber desconto
+        // Compras acima de R$ 350 recebem algum desconto
+        public bool recebeDesconto(float valor)
+        {
+            return (valor >= 350);              
+        }
+
+        /* Retorna o percentual de desconto a ser aplicado:
+         * Valores maiores ou iguais a R$ 5.000,00 ---> 3,0% de desconto
+         * Valores maiores ou iguais a R$ 3.000,00 ---> 2,0% de desconto
+         * Valores maiores ou iguais a R$ 1.000,00 ---> 1,0% de desconto
+         * Valores maiores ou iguais a R$   350,00 ---> 0,5% de desconto */
+        public float percentualDesconto(float valor)
+        {
+            if (valor > 5000) return 0.03f;
+            if (valor > 3000) return 0.02f;
+            if (valor > 1000) return 0.01f;
+            if (valor > 350) return 0.005f;
+            return 0f;
+        }
+
+        // Retorna o valor do desconto aplicado a uma compra
+        public float valorDesconto(float valor)
+        {
+            return valor * percentualDesconto(valor);
+        }
+
+        // Retorna o valor total (final) já considerando desconto (se houver)
+        public float totalCompra(float valor)
+        {
+            return valor - valorDesconto(valor);
+        }
+    }
+}
---
+++ AplicarDesconto/AplicarDescontoTest/DescontoUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using _08_AplicarDesconto;
+
+namespace AplicarDescontoTest
+{
+    [TestClass]
+    public sealed class DescontoUnitTest
+    {
+        private Desconto _desconto;
+
+        [TestInitialize]
+        public void setup() 
+        {
+            _desconto = new Desconto();
+        }
+
+        [TestMethod]
+        [DataRow(380, true)]
+        [DataRow(320, false)]
+        [DataRow(2400, true)]
+        [DataRow(210, false)]
+        [DataRow(2560.50f, true)]
+        [DataRow(120.50f, false)]
+        public void recebeValor(float valor, bool resultadoEsperado)
+        {
+           bool resultadoObtido = _desconto.recebeDesconto(valor);
+
+           Assert.AreEqual(resultadoEsperado, resultadoObtido);
+        }
+
+        [TestMethod]
+        [DataRow(125.00f, 0.0f)]
+        [DataRow(850.00f, 0.5f)]
+        [DataRow(1468.93f, 1.0f)]
+        [DataRow(4655.45f, 2.0f)]
+        [DataRow(8974.60f, 3.0f)]
+        public void retornaPercentualDesconto(float valor, float resultadoEsperado) 
+        {
+            float resultadoObtido = _desconto.percentualDesconto(valor);
+
+            Assert.AreEqual(resultadoEsperado, (resultadoObtido * 100));
+        }
+
+        [TestMethod]
+        [DataRow(125.00f, 0.00f)]
+        [DataRow(850.00f, 4.25f)]
+        [DataRow(1468.93f, 14.69f)]
+        [DataRow(4655.45f, 93.11f)]
+        [DataRow(8974.60f, 269.24f)]
+        public void retornaValorDesconto(float valor, float resultadoEsperado)
+        {
+            float resultadoObtido = _desconto.valorDesconto(valor);
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtido, 2);
+        }
+
+        [TestMethod]
+        [DataRow(125.00f, 125.00f)]
+        [DataRow(850.00f, 845.75f)]
+        [DataRow(1468.93f, 1454.24f)]
+        [DataRow(4655.45f, 4562.34f)]
+        [DataRow(8974.60f, 8705.42f)]
+        public void retornaTotalCompra(float valor, float resultadoEsperado)
+        {
+            float resultadoObtido = _desconto.totalCompra(valor);
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtido, 2);
+        }
+    }
+}
