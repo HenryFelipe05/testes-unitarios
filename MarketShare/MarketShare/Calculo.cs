9263f964bc366f6135a917698f3b04f707c5cd18
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-05-16 21:03:11 -0300

    feat: criando projeto market share

---
+++ MarketShare/MarketShare/Calculo.cs
@@ -0,0 +1,10 @@
+﻿namespace MarketShare
+{
+    public class Calculo
+    {
+        public float calculaMarketShare(float vendas, float total)
+        {
+            return (vendas / total) * 100;
+        }
+    }
+}
